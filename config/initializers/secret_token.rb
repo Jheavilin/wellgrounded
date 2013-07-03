0363d6a2771fb013a2ffcfa25954a9e094281bb9
Justin Heavilin <heavilin@gmail.com>
2013-07-03 14:19:33 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wellgrounded::Application.config.secret_key_base = 'c0dd2ee5937ff405dcc18c715b1786c08ba6f06687bc9073d365166df4bc6e29cb11e0392ce7f36533933817b310c1b4633bf7e4ba5dfb51a69de19fba4319c8'
